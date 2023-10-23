295fa70c387fc6c5d8701ee2ecb3fc2a202f9e42
Sakaev Ruslan <hardist@mail.ru>
2023-10-23 22:11:35 +0300

    Добавил задание к 1 семинару

--- src/seminars/five/number/MaxNumberModule.java
+++ src/seminars/five/number/MaxNumberModule.java
@@ -1,7 +1,18 @@
 package seminars.five.number;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MaxNumberModule {
+    public List<Integer> getList() {
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < count; i++) {
+            list.add(random.nextInt(10));
+        }
+        return list;
+    }
 
 }
