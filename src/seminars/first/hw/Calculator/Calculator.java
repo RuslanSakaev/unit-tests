0c756afb591da000c6a6259ce4de7ad4f4f92160
Sakaev Ruslan <hardist@mail.ru>
2023-10-24 11:28:43 +0300

    Добавил решение задания к 1 семинару

--- src/seminars/first/hw/Calculator/Calculator.java
+++ src/seminars/first/hw/Calculator/Calculator.java
@@ -1,50 +1,33 @@
 package seminars.first.hw.Calculator;
 
 public class Calculator {
-    public static int calculation(int firstOperand, int secondOperand, char operator) {
-        int result;
 
-        switch (operator) {
-            case '+':
-                result = firstOperand + secondOperand;
-                break;
-            case '-':
-                result = firstOperand - secondOperand;
-                break;
-            case '*':
-                result = firstOperand * secondOperand;
-                break;
-            case '/':
-                if (secondOperand != 0) {
-                    result = firstOperand / secondOperand;
-                    break;
-                } else {
-                    throw new ArithmeticException("Division by zero is not possible");
-                }
-            default:
-                throw new IllegalStateException("Unexpected value operator: " + operator);
-        }
-        return result;
-    }
+//    Задание 1. ** В классе Calculator создайте метод calculateDiscount,
+//    который принимает сумму покупки и процент скидки и возвращает сумму с
+//    учетом скидки. Ваша задача - проверить этот метод с использованием библиотеки
+//    AssertJ. Если метод calculateDiscount получает недопустимые аргументы,
+//    он должен выбрасывать исключение ArithmeticException. Не забудьте написать
+//    тесты для проверки этого поведения.
 
-    // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
-    // необходимые проверки для него используя граничные случаи
-    public static double squareRootExtraction(double num) {
-        //  0
-        //  Отрицательные числа
-        //  Дробные значения корней
-        //  Целые
-            if(num < 0) {
-                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
-            }
-            return Math.sqrt(num);
-    }
+    // purchaseAmount - сумма покупки
+    // discountAmount - размер скидки
 
-    // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
-    // Примерная сигнатура и тело метода:
+    // Метод для вычисления суммы покупки со скидкой
     public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
-        // purchaseAmount - сумма покупки
-        // discountAmount - размер скидки
-        return 0; // Метод должен возвращать сумму покупки со скидкой
+        if (purchaseAmount < 0 || discountAmount < 0 || discountAmount > 100) {
+            throw new IllegalArgumentException("Неверный ввод: сумма покупки и сумма скидки должны быть " +
+                    "неотрицательными, а сумма скидки должна находиться в диапазоне от 0 до 100");
+        }
+
+
+        // Рассчитываем сумму со скидкой
+        if (discountAmount == 100) {
+            throw new ArithmeticException("Скидка 100% не допустима");
+        }
+
+        double discount = (double) discountAmount / 100.0;
+
+        return purchaseAmount - (purchaseAmount * discount);
+
     }
 }
