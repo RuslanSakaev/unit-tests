c7450414ac374dd84340d7b6962aa71f9f273680
Sakaev Ruslan <hardist@mail.ru>
2023-10-25 23:05:33 +0300

    Добавил логику в Main

--- src/seminars/second/hw/Main.java
+++ src/seminars/second/hw/Main.java
@@ -21,4 +21,38 @@ public class Main {
              - проверить, что в режиме парковки (сначала testDrive, потом park, т.е эмуляция движения транспорта) машина останавливается (speed = 0)
              - проверить, что в режиме парковки (сначала testDrive, потом park  т.е эмуляция движения транспорта) мотоцикл останавливается (speed = 0)
     */
+    public static void main(String[] args) {
+        // Создание объекта Car
+        Car car = new Car("Honda", "Civic", 2012);
+        System.out.println("Car Details:");
+        System.out.println("Company: " + car.getCompany());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year of Release: " + car.getYearRelease());
+        System.out.println("Number of Wheels: " + car.getNumWheels());
+
+        // Тестовое вождение
+        car.testDrive();
+        System.out.println("Car Speed after Test Drive: " + car.getSpeed());
+
+        // Парковка
+        car.park();
+        System.out.println("Car Speed after Parking: " + car.getSpeed());
+
+        // Создание объекта Motorcycle
+        Motorcycle motorcycle = new Motorcycle("Yamaha", "Bolt", 2017);
+        System.out.println("\nMotorcycle Details:");
+        System.out.println("Company: " + motorcycle.getCompany());
+        System.out.println("Model: " + motorcycle.getModel());
+        System.out.println("Year of Release: " + motorcycle.getYearRelease());
+        System.out.println("Number of Wheels: " + motorcycle.getNumWheels());
+
+        // Тестовое вождение
+        motorcycle.testDrive();
+        System.out.println("Motorcycle Speed after Test Drive: " + motorcycle.getSpeed());
+
+        // Парковка
+        motorcycle.park();
+        System.out.println("Motorcycle Speed after Parking: " + motorcycle.getSpeed());
+    }
+
 }
