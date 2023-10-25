4f687c293707a0eaa50f2496ee34267224c10790
Sakaev Ruslan <hardist@mail.ru>
2023-10-25 22:58:17 +0300

    Написал тесты BeforeEach к 2 семинару

---
+++ Test/seminars/five/SeleniumTest.java
@@ -0,0 +1,5 @@
+package seminars.five;
+
+public class SeleniumTest {
+
+}
--- src/seminars/five/number/MaxNumberModule.java
+++ src/seminars/five/number/MaxNumberModule.java
@@ -1,18 +1,11 @@
 package seminars.five.number;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class MaxNumberModule {
-    public List<Integer> getList() {
-        Random random = new Random();
-        List<Integer> list = new ArrayList<>();
-        int count = 0;
-        for (int i = 0; i < count; i++) {
-            list.add(random.nextInt(10));
-        }
-        return list;
-    }
 
+
+    public int getMaxValue(List<Integer> randomList) {
+        return Collections.max(randomList);
+    }
 }
