0febc3d3c71f8754f965430a49e0851530ccebf6
Ruslan Sakaev <hardist@mail.ru>
2023-11-05 17:50:25 +0300

    правка к 3 семинару

--- Test/seminars/third/hw/EvenOddNumberTest.java
+++ Test/seminars/third/hw/EvenOddNumberTest.java
@@ -1,8 +1,9 @@
 package seminars.third.hw;
 
-import org.junit.BeforeClass;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
